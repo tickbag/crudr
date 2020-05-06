bcfb70bf2de5cefc7b7380d1a2ca2ae70f20cf31
b-hulme <brendan_h5@tickbag.com>
2020-05-06 10:32:39 +0100

    Added tests and fixed convention code.

--- src/CrudR.Api/Conventions/ControllerNameConvention.cs
+++ src/CrudR.Api/Conventions/ControllerNameConvention.cs
@@ -3,18 +3,28 @@ using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace CrudR.Api.Conventions
 {
+    /// <summary>
+    /// Convention to set all controller names to be the provided name in the constructor.
+    /// This should only be used in a project with a single controller.
+    /// </summary>
     public class ControllerNameConvention : IControllerModelConvention
     {
         private readonly string _controllerName;
 
+        /// <summary>
+        /// The Convention Constructor
+        /// </summary>
+        /// <param name="controllerName">The new name for the controller</param>
         public ControllerNameConvention(string controllerName) =>
             _controllerName = controllerName;
 
+        /// <inheritdoc/>
         public void Apply(ControllerModel controller)
         {
             _ = controller ?? throw new ArgumentNullException(nameof(controller));
 
-            controller.ControllerName = _controllerName;
+            if (_controllerName != null)
+                controller.ControllerName = _controllerName;
         }
     }
 }
